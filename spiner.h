54026462050944b59318d1584fa4fba05feee362
lamiaeess <128149604+lamiaeess@users.noreply.github.com>
2023-03-17 13:24:35 +0100

    Add files via upload

---
+++ spiner.h
@@ -0,0 +1,63 @@
+#include <gtk/gtk.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct
+{
+    gint posX;
+    gint posY;
+}Position;
+
+typedef struct 
+{
+    int Hauteur;
+    int Largeur;
+}Taille;
+
+typedef struct {
+    GtkWidget *spinner;
+    GtkWidget *button;
+    Position pos;
+    Taille tai;
+} SpinnerData;
+
+
+ SpinnerData *Init_spinner(GtkWidget *button,gint xpos,gint ypos,gint h,gint l)
+    {
+        SpinnerData *spiner;
+        spiner=(SpinnerData*)malloc(sizeof(SpinnerData));
+        if(!spiner)
+        {
+            printf("\nErreur d allocation[spiner]");
+            exit(0);
+        }
+        spiner->spinner=gtk_spinner_new ();
+         spiner->pos.posX=xpos;
+         spiner->pos.posY=ypos;
+         spiner->tai.Hauteur=h;
+         spiner->tai.Largeur=l;
+         return((SpinnerData*)spiner);
+        
+       
+    }
+
+
+    SpinnerData *creer_spinner()
+    {   
+        SpinnerData *nvsp;
+        if(!nvsp)
+        {
+            printf("\nErreur spinner n'existe pas");
+            exit(0);
+        }
+        nvsp=gtk_spinner_new ();
+        gtk_widget_set_size_request(nvsp->spinner,nvsp->tai.Largeur,nvsp->tai.Hauteur);
+        return((SpinnerData*)nvsp);
+         
+    }
+
+
+
+
+
+
